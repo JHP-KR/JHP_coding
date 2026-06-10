616de2916f9af48414a9df0ce8d4fcd94df88a83
JHP-KR <junghyun5035@gmail.com>
2026-06-10 14:58:11 +0900

    Include metrics for model evaluation in regression example
    Added metrics calculations to evaluate model performance.

--- examples/regression_example.py
+++ examples/regression_example.py
@@ -1,4 +1,5 @@
 from src.linear_regression import SimpleLinearRegression
+from src.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 X = [1, 2, 3, 4, 5]
@@ -12,3 +13,6 @@ predictions = model.predict(X)
 print("slope:", model.slope)
 print("intercept:", model.intercept)
 print("predictions:", predictions)
+print("MSE:", mean_squared_error(y, predictions))
+print("MAE:", mean_absolute_error(y, predictions))
+print("R2:", r2_score(y, predictions))
